bc09f9fd714eaaf2ed01a9f2a6b4905c5276dbd1
blinkev <blinkev@gmail.com>
2019-01-02 22:30:42 +0300

    Remove unused comment

--- data/src/main/java/com/harenko/currencies_test/data/dto/GetCurrenciesResp.kt
+++ data/src/main/java/com/harenko/currencies_test/data/dto/GetCurrenciesResp.kt
@@ -5,43 +5,6 @@ package com.harenko.currencies_test.data.dto
 on 24/12/2018.
  */
 
-data class GetCurrenciesResp(
-        val rates: CurrenciesRatesDto
-) {
-
-    data class CurrenciesRatesDto(
-            val rates: List<CurrencyDto>
-            /*val aud: Double,
-            val bgn: Double,
-            val brl: Double,
-            val cad: Double,
-            val chf: Double,
-            val cny: Double,
-            val czk: Double,
-            val dkk: Double,
-            val gbp: Double,
-            val hkd: Double,
-            val hrk: Double,
-            val huf: Double,
-            val idr: Double,
-            val ils: Double,
-            val inr: Double,
-            val isk: Double,
-            val jpy: Double,
-            val krw: Double,
-            val mxn: Double,
-            val myr: Double,
-            val nok: Double,
-            val nzd: Double,
-            val php: Double,
-            val pln: Double,
-            val ron: Double,
-            val rub: Double,
-            val sek: Double,
-            val sgd: Double,
-            val thb: Double,
-            val `try`: Double,
-            val usd: Double,
-            val zar: Double*/
-    )
+data class GetCurrenciesResp(val rates: CurrenciesRatesDto) {
+    data class CurrenciesRatesDto(val rates: List<CurrencyDto>)
 }
